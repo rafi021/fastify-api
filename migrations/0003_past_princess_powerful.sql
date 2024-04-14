d52fb24d27d362ee393e4f7f0eaa05cf6ca21387
Mahmud Ibrahim <mahmud.ibrahim021@gmail.com>
2024-04-14 23:35:20 +0600

    define schemas

---
+++ migrations/0003_past_princess_powerful.sql
@@ -0,0 +1,26 @@
+CREATE TABLE IF NOT EXISTS "usersToRoles" (
+	"applicationId" uuid NOT NULL,
+	"userId" uuid NOT NULL,
+	"roleId" uuid NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "usersToRoles_applicationId_roleId_userId_pk" PRIMARY KEY("applicationId","roleId","userId")
+);
+
+DO $$ BEGIN
+ ALTER TABLE "usersToRoles" ADD CONSTRAINT "usersToRoles_applicationId_applications_id_fk" FOREIGN KEY ("applicationId") REFERENCES "applications"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+
+DO $$ BEGIN
+ ALTER TABLE "usersToRoles" ADD CONSTRAINT "usersToRoles_userId_users_id_fk" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+
+DO $$ BEGIN
+ ALTER TABLE "usersToRoles" ADD CONSTRAINT "usersToRoles_roleId_roles_id_fk" FOREIGN KEY ("roleId") REFERENCES "roles"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,66 @@
-import { pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  primaryKey,
+  text,
+  timestamp,
+  uniqueIndex,
+  uuid,
+  varchar,
+} from "drizzle-orm/pg-core";
 
-export const application = pgTable("applications", {
+export const applications = pgTable("applications", {
   id: uuid("id").primaryKey().defaultRandom(),
   name: varchar("name", { length: 256 }).notNull(),
   created_at: timestamp("created_at").defaultNow().notNull(),
   updated_at: timestamp("updated_at").defaultNow().notNull(),
+});
+
+export const users = pgTable(
+  "users",
+  {
+    id: uuid("id").defaultRandom().notNull(),
+    name: varchar("name", { length: 256 }).notNull(),
+    email: varchar("email", { length: 256 }).notNull(),
+    applicationId: uuid("applicationId").references(() => applications.id),
+    password: varchar("password", { length: 256 }).notNull(),
+    created_at: timestamp("created_at").defaultNow().notNull(),
+    updated_at: timestamp("updated_at").defaultNow().notNull(),
+  },
+  (users) => {
+    return {
+      cpk: primaryKey({ columns: [users.email, users.applicationId] }),
+      idIndex: uniqueIndex("users_id_index").on(users.id),
+    };
+  }
+);
+
+export const roles = pgTable(
+  "roles",
+  {
+    id: uuid("id").defaultRandom().notNull(),
+    name: varchar("name", { length: 256 }).notNull(),
+    applicationId: uuid("applicationId").references(() => applications.id),
+    permissions: text("permissions").array().$type<Array<string>>(),
+    created_at: timestamp("created_at").defaultNow().notNull(),
+    updated_at: timestamp("updated_at").defaultNow().notNull(),
+  },
+  (roles) => {
+    return {
+      cpk: primaryKey({ columns: [roles.name, roles.applicationId] }),
+      idIndex: uniqueIndex("roles_id_index").on(roles.id),
+    };
+  }
+);
+
+export const usersToRoles = pgTable("usersToRoles", {
+    applicationId: uuid("applicationId").references(() =>applications.id).notNull(),
+    userId: uuid("userId").references(() => users.id).notNull(),
+    roleId: uuid("roleId").references(() => roles.id).notNull(),
+    created_at: timestamp("created_at").defaultNow().notNull(),
+    updated_at: timestamp("updated_at").defaultNow().notNull(),
+
+}, (usersToRoles) => {
+    return {
+        cpk: primaryKey({ columns: [usersToRoles.applicationId, usersToRoles.roleId, usersToRoles.userId] }),
+    };
 });
